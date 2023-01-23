c00d9838fe499cccc57d71151f7e39c5736dfd27
Ajay Singh <ajay10mar96@gmail.com>
2023-01-23 12:15:47 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Store</h1>
+      <div className="card">
+          <img src="/store-icon-dark-mode.svg" alt="store icon" style={{width: 200}}/> 
+        <p>
+          Store App Frontend
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/config/env.ts
@@ -0,0 +1,3 @@
+export const APP_PORT = 5173;
+export const APP_URL = `http://localhost:${APP_PORT}`;
+export const API_URL = `http://localhost:3000`;
